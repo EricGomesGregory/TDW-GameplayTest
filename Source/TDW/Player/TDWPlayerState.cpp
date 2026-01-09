57546633defc4c38fcff4b008b6bf01e36224ed5
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2026-01-09 10:00:37 -0300

    Update Player classes
    Create project PlayerState class

---
+++ Source/TDW/Player/TDWPlayerState.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TDWPlayerState.h"
+
+#include "TDW/AbilitySystem/TDWAbilitySystemComponent.h"
+#include "TDW/AbilitySystem/Attributes/TDWCombatSet.h"
+#include "TDW/AbilitySystem/Attributes/TDWHealthSet.h"
+#include "TDW/AbilitySystem/Attributes/TDWManaSet.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TDWPlayerState)
+
+
+ATDWPlayerState::ATDWPlayerState(const FObjectInitializer& ObjectInitializer)
+{
+	AbilitySystemComponent = ObjectInitializer.CreateDefaultSubobject<UTDWAbilitySystemComponent>(this, TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	// These attribute sets will be detected by AbilitySystemComponent::InitializeComponent. Keeping a reference so that the sets don't get garbage collected before that.
+	HealthSet = CreateDefaultSubobject<UTDWHealthSet>(TEXT("HealthSet"));
+	ManaSet = CreateDefaultSubobject<UTDWManaSet>(TEXT("ManaSet"));
+	CombatSet = CreateDefaultSubobject<UTDWCombatSet>(TEXT("CombatSet"));
+
+	SetNetUpdateFrequency(100.0f);
+}
+
+UAbilitySystemComponent* ATDWPlayerState::GetAbilitySystemComponent() const
+{
+	return GetTDWAbilitySystemComponent();
+}
---
+++ Source/TDW/Player/TDWPlayerState.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "TDWPlayerState.generated.h"
+
+class UTDWAbilitySystemComponent;
+class UTDWCombatSet;
+class UTDWHealthSet;
+class UTDWManaSet;
+
+
+/**
+ * 
+ */
+UCLASS(Config = Game)
+class TDW_API ATDWPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	ATDWPlayerState(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UFUNCTION(BlueprintCallable, Category = "TDW|PlayerState")
+	UTDWAbilitySystemComponent* GetTDWAbilitySystemComponent() const { return AbilitySystemComponent; }
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	
+private:
+
+	// The ability system component sub-object used by player characters.
+	UPROPERTY(VisibleAnywhere, Category = "TDW|PlayerState")
+	TObjectPtr<UTDWAbilitySystemComponent> AbilitySystemComponent;
+
+	// Health attribute set used by this actor.
+	UPROPERTY()
+	TObjectPtr<const UTDWHealthSet> HealthSet;
+
+	// Mana attribute set used by this actor.
+	UPROPERTY()
+	TObjectPtr<const UTDWManaSet> ManaSet;
+	
+	// Combat attribute set used by this actor.
+	UPROPERTY()
+	TObjectPtr<const UTDWCombatSet> CombatSet;
+};
